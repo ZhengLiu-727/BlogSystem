62bc6e47728afcd01ca2eb732d6b2d6abbe6ad9d
Zheng Liu <zhengliu.zhengliu@gmail.com>
2021-12-14 20:24:49 +0800

    [style] Optimize coding style
    Follow the Alibaba Java Coding Guidelines

--- blog-parent/blog/src/main/java/site/liuzheng/config/MybatisPlusConfig.java
+++ blog-parent/blog/src/main/java/site/liuzheng/config/MybatisPlusConfig.java
@@ -6,10 +6,16 @@ import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+/**
+ * @author liuzheng
+ */
 @Configuration
 @MapperScan("site.liuzheng.dao.mapper")
 public class MybatisPlusConfig {
-    // 分页插件
+
+    /**
+     * 分页插件
+     */
     @Bean
     public MybatisPlusInterceptor mybatisPlusInterceptor(){
         MybatisPlusInterceptor interceptor = new MybatisPlusInterceptor();
--- blog-parent/blog/src/main/java/site/liuzheng/config/WebMVCConfig.java
+++ blog-parent/blog/src/main/java/site/liuzheng/config/WebMVCConfig.java
@@ -4,10 +4,16 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
+/**
+ * @author liuzheng
+ */
 @Configuration
 public class WebMVCConfig implements WebMvcConfigurer {
 
-    // 跨域配置
+    /**
+     * 跨域配置
+     * @param registry
+     */
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**").allowedOrigins("http://localhost:8080");
