9e082ef3dccff3938273df9bfb13ac2a186b7f81
Zheng Liu <zhengliu.zhengliu@gmail.com>
2022-02-21 20:34:51 +0800

    [feat] LoginInterceptor

--- blog-parent/blog/src/main/java/site/liuzheng/config/WebMVCConfig.java
+++ blog-parent/blog/src/main/java/site/liuzheng/config/WebMVCConfig.java
@@ -1,14 +1,20 @@
 package site.liuzheng.config;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import site.liuzheng.handler.LoginInterceptor;
 
 /**
  * @author liuzheng
  */
 @Configuration
 public class WebMVCConfig implements WebMvcConfigurer {
+
+    @Autowired
+    private LoginInterceptor loginInterceptor;
 
     /**
      * 跨域配置
@@ -17,5 +23,14 @@ public class WebMVCConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**").allowedOrigins("http://localhost:8080");
+    }
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        //拦截test接口，后续实际遇到需要拦截的接口时，在配置为真正的拦截接口
+        registry.addInterceptor(loginInterceptor)
+                .addPathPatterns("/test")
+                .addPathPatterns("/comments/create/change")
+                .addPathPatterns("/articles/publish");
     }
 }
